07877b200f63d8c53aacf43be6d4c0e2b40d36a2
TDonVitorio420 <victor.pinedamo@amigo.edu.co>
2025-10-15 22:52:09 -0500

    Primer commit - React Netlify Demo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      backgroundColor: "#282c34",
+      color: "white",
+      fontSize: "2rem",
+      fontFamily: "Arial, sans-serif"
+    }}>
+      React with Netlify CI CD pipeline
+    </div>
+  );
+}
+
+export default App;
