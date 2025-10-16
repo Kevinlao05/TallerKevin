f3ac02412e86bf6510f7a7f5d176ecd804d0f40b
TDonVitorio420 <victor.pinedamo@amigo.edu.co>
2025-10-16 00:57:58 -0500

    Actualizo texto final del taller

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 function App() {
   return (
-    <div style={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "100vh",
-      backgroundColor: "#282c34",
-      color: "white",
-      fontSize: "2rem",
-      fontFamily: "Arial, sans-serif"
-    }}>
-      React with Netlify CI CD pipeline
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "#282c34",
+        color: "white",
+        fontSize: "1.5rem",
+        fontFamily: "Arial, sans-serif",
+        textAlign: "center",
+        flexDirection: "column",
+        padding: "2rem",
+        lineHeight: "2.2rem",
+      }}
+    >
+      <h1 style={{ fontSize: "2rem", marginBottom: "1rem" }}>
+        Taller de aplicación de prácticas de desarrollo
+      </h1>
+
+      <p>
+        mediante <strong>React</strong> con <strong>Vite</strong>,{" "}
+        <strong>GitHub</strong> y <strong>Netlify</strong>.
+      </p>
+
+      <p>
+        Con el fin de simular un entorno de trabajo real con enfoque{" "}
+        <strong>DevOps</strong> y <strong>CI/CD</strong>.
+      </p>
+
+      <br />
+
+      <p>
+        <strong>Estudiante:</strong> Victor Andres Pineda Mosquera
+      </p>
+      <p>
+        <strong>Clase:</strong> Actualización y Mantenimiento de Software
+      </p>
     </div>
   );
 }
